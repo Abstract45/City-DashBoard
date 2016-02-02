f60c465648dd1a6c73830ee2bba1939f9bbceb3a
Alex-Besson <alex_j67@hotmail.com>
2016-02-02 17:11:19 -0500

    Readded news api files.

---
+++ City-Dashboard/City-Dashboard/News.swift
@@ -0,0 +1,190 @@
+//
+//  News.swift
+//  News API
+//
+//  Created by Alexander Besson on 2016-02-02.
+//  Copyright © 2016 Alexander Besson. All rights reserved.
+//
+
+import Foundation
+
+class News {
+    
+    var news = [NewsItem]()
+    
+    typealias DownloadComplete = () -> ()
+    private var KEY_API = "3ba12a165e9410d1b2dbe69ec21e26f6:17:74262608"
+    
+    init() {
+        
+    }
+    
+    func downloadNews(completion: DownloadComplete) {
+        
+        let urlString = "http://api.nytimes.com/svc/topstories/v1/home.json?api-key=3ba12a165e9410d1b2dbe69ec21e26f6:17:74262608"
+        
+        let url = NSURL(string: urlString)
+        
+        let _ = NSURLSession.sharedSession().dataTaskWithURL(url!) { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                
+                do {
+                    
+                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments)
+                    
+                    if let newsDict = json as? Dictionary<String, AnyObject> {
+                        
+                        if let results = newsDict["results"] as? Array<AnyObject> {
+                            
+                            for result in results {
+                                
+                                let newsItem = NewsItem()
+                                
+                                if let r = result as? Dictionary<String, AnyObject> {
+                                    
+                                    if let ttl = r["title"] as? String {
+                                        newsItem.title = ttl
+                                    }
+                                    
+                                    if let sec = r["section"] as? String {
+                                        newsItem.section = sec
+                                    }
+                                    
+                                    if let subSec = r["subsection"] as? String {
+                                        newsItem.subSection = subSec
+                                    }
+                                    
+                                    if let abs = r["abstract"] as? String {
+                                        newsItem.abstract = abs
+                                    }
+                                    
+                                    if let urlStr = r["url"] as? String {
+                                        newsItem.url = urlStr
+                                    }
+                                    
+                                    if let byln = r["byline"] as? String {
+                                        newsItem.author = byln
+                                    }
+                                    
+                                    if let uDate = r["updated_date"] as? String {
+                                        newsItem.dateUpdated = uDate
+                                    }
+                                    
+                                    if let cDate = r["created_date"] as? String {
+                                        newsItem.dateCreated = cDate
+                                    }
+                                    
+                                    if let pDate = r["published_date"] as? String {
+                                        newsItem.datePublished = pDate
+                                    }
+                                    
+                                    if let kik = r["kicker"] as? String {
+                                        newsItem.kicker = kik
+                                    }
+                                    
+                                    if let desF = r["des_facet"] as? Array<String> {
+                                        
+                                        newsItem.facetDescription.removeAll()
+                                        
+                                        for df in desF {
+                                            
+                                            newsItem.facetDescription.append(df)
+                                        }
+                                    }
+                                    
+                                    if let orgF = r["org_facet"] as? Array<String> {
+                                        
+                                        newsItem.facetOrganization.removeAll()
+                                        
+                                        for of in orgF {
+                                            
+                                            newsItem.facetOrganization.append(of)
+                                        }
+                                    }
+                                    
+                                    if let perF = r["per_facet"] as? Array<String> {
+                                        
+                                        newsItem.facetPerson.removeAll()
+                                        
+                                        for pf in perF {
+                                            
+                                            newsItem.facetPerson.append(pf)
+                                        }
+                                    }
+                                    
+                                    if let geoF = r["geo_facet"] as? Array<String> {
+                                        
+                                        newsItem.facetGeo.removeAll()
+                                        
+                                        for gf in geoF {
+                                            
+                                            newsItem.facetGeo.append(gf)
+                                        }
+                                    }
+                                    
+                                    if let multiM = r["multimedia"] as? Array<AnyObject> {
+                                        
+                                        newsItem.multimedia.removeAll()
+                                        
+                                        for mm in multiM {
+                                            
+                                            let mMedia = Multimedia()
+                                            
+                                            if let m = mm as? Dictionary<String, AnyObject> {
+                                                
+                                                if let urlStr = m["url"] as? String {
+                                                    mMedia.url = urlStr
+                                                }
+                                                
+                                                if let form = m["format"] as? String {
+                                                    mMedia.format = form
+                                                }
+                                            
+                                                if let hi = m["height"] as? Int {
+                                                    mMedia.height = hi
+                                                }
+                                                
+                                                if let wi = m["width"] as? Int {
+                                                    mMedia.width = wi
+                                                }
+                                                
+                                                if let typ = m["type"] as? String {
+                                                    mMedia.type = typ
+                                                }
+                                                
+                                                if let styp = m["subtype"] as? String {
+                                                    mMedia.subtype = styp
+                                                }
+                                                
+                                                if let cap = m["caption"] as? String {
+                                                    mMedia.caption = cap
+                                                }
+                                                
+                                                if let cRight = m["copyright"] as? String {
+                                                    mMedia.copyright = cRight
+                                                }
+                                            }
+                                            
+                                            newsItem.multimedia.append(mMedia)
+                                            
+                                        }
+                                    }
+                                    
+                                    self.news.append(newsItem)
+                                }
+                            }
+                        }
+                    }
+                    
+                } catch {
+                    print("Could not serialize JSON for news api.")
+                }
+                
+                completion()
+                
+            })
+            
+        }.resume()
+    }
+}
---
+++ City-Dashboard/City-Dashboard/NewsItem.swift
@@ -0,0 +1,235 @@
+//
+//  NewsItem.swift
+//  News API
+//
+//  Created by Alexander Besson on 2016-02-02.
+//  Copyright © 2016 Alexander Besson. All rights reserved.
+//
+
+import Foundation
+
+class NewsItem {
+    
+    private var _title: String?
+    private var _section: String?
+    
+    var title: String {
+        get {
+            if _title != nil {
+                return _title!
+            } else {
+                return ""
+            }
+        }
+        set(newValue) {
+            _title = newValue
+        }
+    }
+    
+    var section: String {
+        get {
+            if _section != nil {
+                return _section!
+            } else {
+                return ""
+            }
+        }
+        set(newValue) {
+            _section = newValue
+        }
+    }
+    
+    // description
+    private var _abstract: String?
+    private var _kicker: String?
+    private var _subSection: String?
+    
+    var abstract: String {
+        get {
+            if _abstract != nil {
+                return _abstract!
+            } else {
+                return ""
+            }
+        }
+        set(newValue) {
+            _abstract = newValue
+        }
+    }
+    
+    var kicker: String {
+        get {
+            if _kicker != nil {
+                return _kicker!
+            } else {
+                return ""
+            }
+        }
+        set(newValue) {
+            _kicker = newValue
+        }
+    }
+
+    var subSection: String {
+        get {
+            if _subSection != nil {
+                return _subSection!
+            } else {
+                return ""
+            }
+        }
+        set(newValue) {
+            _subSection = newValue
+        }
+    }
+    
+    // article info
+    private var _url: String?
+    private var _author: String?
+    private var _dateCreated: String?
+    private var _datePublished: String?
+    private var _dateUpdated: String?
+    
+    var url: String {
+        get {
+            if _url != nil {
+                return _url!
+            } else {
+                return ""
+            }
+        }
+        set(newValue) {
+            _url = newValue
+        }
+    }
+    
+    var author: String {
+        get {
+            if _author != nil {
+                return _author!
+            } else {
+                return ""
+            }
+        }
+        set(newValue) {
+            _author = newValue
+        }
+    }
+    
+    var dateCreated: String {
+        get {
+            if _dateCreated != nil {
+                return _dateCreated!
+            } else {
+                return ""
+            }
+        }
+        set(newValue) {
+            _dateCreated = newValue
+        }
+    }
+    
+    var datePublished: String {
+        get {
+            if _datePublished != nil {
+                return _datePublished!
+            } else {
+                return ""
+            }
+        }
+        set(newValue) {
+            _datePublished = newValue
+        }
+    }
+    
+    var dateUpdated: String {
+        get {
+            if _dateUpdated != nil {
+                return _dateUpdated!
+            } else {
+                return ""
+            }
+        }
+        set(newValue) {
+            _dateUpdated = newValue
+        }
+    }
+    
+    // facets
+    private var _facetDescription: [String]?
+    private var _facetOrganization: [String]?
+    private var _facetPerson: [String]?
+    private var _facetGeo: [String]?
+    
+    var facetDescription: [String] {
+        get {
+            if _facetDescription != nil {
+                return _facetDescription!
+            } else {
+                return [""]
+            }
+        }
+        set(newValue) {
+            _facetDescription = newValue
+        }
+    }
+    
+    var facetOrganization: [String] {
+        get {
+            if _facetOrganization != nil {
+                return _facetOrganization!
+            } else {
+                return [""]
+            }
+        }
+        set(newValue) {
+            _facetOrganization = newValue
+        }
+    }
+    
+    var facetPerson: [String] {
+        get {
+            if _facetPerson != nil {
+                return _facetPerson!
+            } else {
+                return [""]
+            }
+        }
+        set(newValue) {
+            _facetPerson = newValue
+        }
+    }
+    
+    var facetGeo: [String] {
+        get {
+            if _facetGeo != nil {
+                return _facetGeo!
+            } else {
+                return [""]
+            }
+        }
+        set(newValue) {
+            _facetGeo = newValue
+        }
+    }
+    
+    // multimedia
+    private var _multimedia: [Multimedia]?
+    
+    var multimedia: [Multimedia] {
+        get {
+            if _multimedia != nil {
+                return _multimedia!
+            } else {
+                return [Multimedia()]
+            }
+        }
+        set(newValue) {
+            _multimedia = newValue
+        }
+    }
+    
+    init() {
+        
+    }
+}
